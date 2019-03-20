84acaf8a6b108b8d415939bb22db42f1225fc224
sergeyokten <sergey.okten@gmail.com>
2019-03-20 17:28:47 +0200

    asdqwe

--- presentations/demo1/main.js
+++ presentations/demo1/main.js
@@ -1,18 +1,65 @@
-console.log('this is external script file log');
+// console.log('this is external script file log');
+//
+//
+// var a; // декларація
+// a = 100; // ініціалізація
+//
+// let b = 'hello oktenweb'; // декларація + ініціалізація
+//
+// console.log(a); // 100
+// console.log(b); // hello oktenweb
+//
+// {
+//     var c = 'this is var';
+//     let d = 'this is let';
+// }
+// console.log(c); // this is var
+// console.log(d); // error!
+//
+// // const PI = 3.14;
+// // PI = 3.15;
+//
+// let num = 100500;
+// console.log(typeof num); // number
+// let double = 3.14;
+// console.log(typeof double); // number
+// let str = 'hello oktenweb';
+// console.log(typeof str); // string
+// let bool1 = true;
+// console.log(typeof bool1); // boolean
+// let bool2 = false;
+// console.log(typeof bool2); // boolean
+// let array = ['hello', 'oktenweb', str, 123];
+// console.log(typeof array); // object
+// let user = {name: 'Jack', surname: 'Sparrow', age: 34};
+// console.log(typeof user); // object
+// let undef = undefined;
+// console.log(typeof undef); // undefined
+// let not_a_number = NaN;
+// console.log(typeof not_a_number);// number
+//
+// let someVar;
+// console.log(someVar); // undefined
 
 
-var a; // декларація
-a = 100; // ініціалізація
 
-let b = 'hello oktenweb'; // декларація + ініціалізація
 
-console.log(a); // 100
-console.log(b); // hello oktenweb
 
-{
-    var c = 'this is var';
-    let d = 'this is let';
-}
-console.log(c); // this is var
-console.log(d); // error!
+
+
+let a = 12;
+let b = 5;
+let result1 = a + b; // 17
+let result2 = a - b; // 7
+let result3 = a * b; // 60
+let result4 = a / b; // 2.4
+let result5 = a % b; // 2
+
+a = a + 1; // 13
+a += 1; // 14
+a++; // 15
+++a; //16
+
+let x = ++a; // x = 17
+let y = b++; // y = 5
 
