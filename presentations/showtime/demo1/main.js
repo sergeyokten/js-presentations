f08e8107aae114e3931fc6ec1d7293309ffb1f5a
sergeyokten <sergey.okten@gmail.com>
2019-03-20 19:30:27 +0200

    asdqwe

---
+++ presentations/showtime/demo1/main.js
@@ -0,0 +1,79 @@
+// // console.log('hello oktenweb');
+// //
+// // var a;
+// // a = 100;
+// // console.log(a);
+// //
+// // let b = 'hello oktenweb';
+// // console.log(b);
+//
+//
+// {
+//     var c = 'this is var';
+//     let d = 'this is let';
+// }
+//
+//
+// // console.log(c);
+// // console.log(d); // error!
+//
+// const PI = 3.14;
+// // PI = 2.7;
+// console.log(PI);
+//
+// let num = 100500;
+// console.log(typeof num);
+// let double = 3.14;
+// console.log(typeof double);
+// let str = 'oktenweb';
+// console.log(typeof str);
+// let bool1 = true;
+// console.log(typeof bool1);
+// let bool2 = false;
+// console.log(typeof bool2);
+// let array = ['hello', 'oktenweb', str, 123];
+// console.log(typeof array);
+// let userObject = {name: 'Jack', surname: 'Doe', age: 34};
+// console.log(typeof userObject);
+//
+// let undef = undefined;
+// console.log(undef);
+// console.log(typeof undef);
+// let not_a_number = NaN;
+// console.log(typeof not_a_number);
+// let nn = null;
+// console.log(typeof nn);
+//
+
+
+let a = 12;
+let b = 5;
+let result1 = a + b;
+console.log(result1);
+let result2 = a - b;
+console.log(result2);
+let res3 = a * b;
+console.log(res3);
+let res4 = a / b;
+console.log(res4);
+let res5 = a % b;
+console.log(res5);
+
+a = a + 1;
+console.log(a);
+a += 1;
+console.log(a);
+a++;
+console.log(a);
+++a;
+console.log(a);
+
+let x = a++;
+console.log(x);
+
+let num = 100;
+let str = 'oktenweb';
+let res = str + num + 200;
+console.log(typeof res);
+
+
